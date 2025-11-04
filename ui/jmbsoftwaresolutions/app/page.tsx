34a9b9c660555ac9946e924f4904e77e1d5825f6
James Bales <james@otwebdesigns.com>
2025-11-04 18:00:02 -0500

    Set dark styling

--- ui/jmbsoftwaresolutions/app/page.tsx
+++ ui/jmbsoftwaresolutions/app/page.tsx
@@ -8,7 +8,8 @@ export default function Home() {
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-            <div className="flex gap-5 items-center font-semibold">
+            <div className="flex gap-5 items-center font-semibold text-lg">
+              JMB Software Solutions
               
             </div>
             <AuthButton />
